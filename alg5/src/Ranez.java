cdbbb1dca7a36a346b29ebc974c3ba350729d8ca
OzzyFrost <40811357+OzzyFrost@users.noreply.github.com>
2018-08-09 17:53:20 +0500

    Add files via upload

---
+++ alg5/src/Ranez.java
@@ -0,0 +1,11 @@
+public class Ranez {
+    int n = 5;
+    int w = 10;
+    int[][] arr = new int[n][2];
+    public static void main(String[] args) {
+
+    }
+    public static int maxW(int n,int w){
+        
+    }
+}
